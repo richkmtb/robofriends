ad86362580a5f5df1a0ff2d303e7ebdc98a8a361
RichK <richkmtb@hotmail.com>
2019-01-07 17:15:36 +0000

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'James Kiernan',
+	username: 'JimK',
+	email: 'jimjjim@hotmail.com'
+},
+{
+	id: 2,
+	name: 'John Grant',
+	username: 'Bags',
+	email: 'gayforbags@hotmail.com'
+},
+{
+	id: 3,
+	name: 'Chris Mil',
+	username: 'GrannySHag',
+	email: 'granniesplease@hotmail.com'
+},
+{
+	id: 4,
+	name: 'Tarnya Coulson',
+	username: 'Tarny',
+	email: 'Tarnyatc@hotmail.com'
+},
+{
+	id: 5,
+	name: 'Edie K',
+	username: 'Pooper',
+	email: 'EdieK@hotmail.com'
+},
+{
+	id: 6,
+	name: 'Ray Webster',
+	username: 'Smellz',
+	email: 'Stinky@hotmail.com'
+},
+{
+	id: 7,
+	name: 'Adam carrier',
+	username: 'the fiddler',
+	email: 'Acarrier@hotmail.com'
+},
+{
+	id: 8,
+	name: 'Alex Strongman',
+	username: 'Alice Strongbow',
+	email: 'THS@hotmail.com'
+},
+{
+	id: 9,
+	name: 'Roberty Brighton',
+	username: 'Rob',
+	email: 'jewishmonkey@hotmail.com'
+},
+{
+	id: 10,
+	name: 'Lee Matthews', 
+	username: 'Lee',
+	email: 'leematthews@hotmail.com'
+},
+]
