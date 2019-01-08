f95c04e77aa674ce0bef2c339f72539c656934c3
RichK <richkmtb@hotmail.com>
2019-01-08 13:19:05 +0000

    Updates

---
+++ precache-manifest.65e8804e54b3b3ba8eee7d2b4ba8e746.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "36c119079316deb43864",
+    "url": "/robofriends/static/js/main.36c11907.chunk.js"
+  },
+  {
+    "revision": "4c826f545589ffe21429",
+    "url": "/robofriends/static/js/1.4c826f54.chunk.js"
+  },
+  {
+    "revision": "36c119079316deb43864",
+    "url": "/robofriends/static/css/main.6868047a.chunk.css"
+  },
+  {
+    "revision": "4c826f545589ffe21429",
+    "url": "/robofriends/static/css/1.ec4bbbff.chunk.css"
+  },
+  {
+    "revision": "6915b571bf9202044bf5d63a8865a3ec",
+    "url": "/robofriends/index.html"
+  }
+];
